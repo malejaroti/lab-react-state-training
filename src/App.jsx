fed8b2402a015df4a90c79db9b4f28f1af728958
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-08-15 08:44:11 +0200

    Bonus Iteration 6: Caroulse done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Carousel from "./components/Carousel";
 import ClickablePicture from "./components/ClickablePicture";
 import Counter from "./components/Counter";
 import Dice from "./components/Dice";
@@ -23,6 +24,15 @@ function App() {
       <DiscoButton />
       <DiscoButton />
       </div>
+
+      <Carousel
+        images={[
+          "https://randomuser.me/api/portraits/women/1.jpg",
+          "https://randomuser.me/api/portraits/men/1.jpg",
+          "https://randomuser.me/api/portraits/women/2.jpg",
+          "https://randomuser.me/api/portraits/men/2.jpg"
+        ]}
+      />
     </div>
   );
 }
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+function Carousel({images}) {
+    const [imageIndex, setImageIndex] = useState(0)
+    const handleClickRight = () => {
+        setImageIndex(imageIndex+1)
+    }
+    const handleClickLeft = () => {
+        imageIndex === 0 ? setImageIndex(images.length- 1) : setImageIndex(imageIndex-1)
+    }
+
+
+  return (
+
+    <div className="carousel">
+        <button onClick={handleClickLeft}>Left</button>
+        <img src={images[imageIndex]} alt="" />
+        <button onClick={handleClickRight}>Right</button>
+    </div>
+  )
+}
+export default Carousel
